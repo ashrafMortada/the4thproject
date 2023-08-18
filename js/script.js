85bb16e03942a4cd07527f496a5ff10475fd4768
ashrafMortada <ashrafgeology123@gmail.com>
2023-08-18 23:29:39 +0300

    the 4th project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+document.getElementById("bookbtn1").onclick=function(){
+    alert(" تم الحجز ")
+    prompt ("  برجاء ادخال رقم الهاتف للمتابعة ...")
+}
+
+document.getElementById("bookbtn2").onclick=function(){
+    alert(" تم الحجز ")
+    prompt ("  برجاء ادخال رقم الهاتف للمتابعة ...")
+
+}
+document.getElementById("knowbtn").onclick=function(){
+    alert(" furniture.com يمكنك زيارة موقعنا لمعرفة المزيد ")
+}
+document.getElementById("copy").onclick=function(){
+    alert("URL تم نسخ عنوان")   
+}
